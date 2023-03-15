69aab90a051ba172446f0648e27e198db309deee
Burak Başaran <111579457+BurakBsrn@users.noreply.github.com>
2023-03-16 00:34:44 +0300

    Add files via upload

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        
+        tekrar:
+            Console.Write("Mail Adresinizi Giriniz... ");
+            string mail = Console.ReadLine();
+
+            if (mail.Contains("@") == true) 
+                Console.Write("Kullanıcı kaydı yapılmıştır...");
+            else
+            {
+                Console.Write("Doğru bir mail adresi giriniz...");
+                goto tekrar;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
